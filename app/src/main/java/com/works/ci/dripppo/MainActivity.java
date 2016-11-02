ca730ddf0b79a7811d8736883a460f0eda05f761
Jason <j1994d@gmail.com>
2016-11-02 20:38:58 +0800

    將API請求跟UI thread分開

--- app/src/main/java/com/works/ci/dripppo/MainActivity.java
+++ app/src/main/java/com/works/ci/dripppo/MainActivity.java
@@ -1,14 +1,17 @@
 package com.works.ci.dripppo;
 
 import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import com.works.ci.dripppo.API.apiService;
 import com.works.ci.dripppo.Item.Shot;
@@ -16,14 +19,55 @@ import com.works.ci.dripppo.Item.Shot;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private MyAdapter myAdapter;
+    private ArrayList<Shot> shotArrayList;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView= (ListView)findViewById(R.id.listView);
 
+        listView= (ListView)findViewById(R.id.listView);
+        shotArrayList = new ArrayList<Shot>();
+        myAdapter = new MyAdapter(this, shotArrayList);
+        listView.setAdapter(myAdapter);
+
+        handler = new Handler() {
+
+
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                Gson gson = new Gson();
+                Type mapType = new TypeToken< ArrayList<Shot> >() {}.getType();
+                shotArrayList = gson.fromJson((String) msg.obj, mapType);
+                myAdapter.refresh(shotArrayList);
+            }
+        };
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    apiService apitest = new apiService();
+                    String response = apitest.get("https://api.dribbble.com/v1/shots/?access_token=82b6a13113f4406633b6b7f44972a7a74fa4578fd3f08e4f92a80f65690fd004");
+                    Message message = new Message();
+                    message.obj = response;
+
+                    Thread.sleep(3000);
+                    handler.sendMessage(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+/*
         apiService apitest = new apiService();
         String response = apitest.get("https://api.dribbble.com/v1/shots/?access_token=82b6a13113f4406633b6b7f44972a7a74fa4578fd3f08e4f92a80f65690fd004");
 
@@ -34,16 +78,18 @@ public class MainActivity extends AppCompatActivity {
         final MyAdapter adapter = new MyAdapter(this, listItem);
         listView.setAdapter(adapter);
 
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Shot temp = (Shot)adapter.getItem(position);
+                myAdapter.notifyDataSetChanged();
+                Shot temp = (Shot)myAdapter.getItem(position);
 
                 Intent intent = new Intent(MainActivity.this, ShotInformation.class);
                 intent.putExtra("url", temp.getHtml_url());
                 startActivity(intent);
             }
-        });
+        });*/
     }
 
 }
