4e5974a4a25673ae1cb9e509f50ccfe650debd84
Jason <j1994d@gmail.com>
2016-11-03 01:20:08 +0800

    新增footerView

--- app/src/main/java/com/works/ci/dripppo/MainActivity.java
+++ app/src/main/java/com/works/ci/dripppo/MainActivity.java
@@ -1,14 +1,18 @@
 package com.works.ci.dripppo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -26,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private MyAdapter myAdapter;
     private ArrayList<Shot> shotArrayList;
     private Handler handler;
+    private boolean isloading = false;
+    private View footerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,36 +43,39 @@ public class MainActivity extends AppCompatActivity {
         myAdapter = new MyAdapter(this, shotArrayList);
         listView.setAdapter(myAdapter);
 
+        //LayoutInflater li = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //ftView = li.inflate(R.layout.footer_view, null);
+        LayoutInflater layoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        footerView = layoutInflater.inflate(R.layout.shot_footer, null);
+
         handler = new Handler() {
 
 
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
-                Gson gson = new Gson();
-                Type mapType = new TypeToken< ArrayList<Shot> >() {}.getType();
-                shotArrayList = gson.fromJson((String) msg.obj, mapType);
-                myAdapter.refresh(shotArrayList);
-            }
-        };
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    apiService apitest = new apiService();
-                    String response = apitest.get("https://api.dribbble.com/v1/shots/?access_token=82b6a13113f4406633b6b7f44972a7a74fa4578fd3f08e4f92a80f65690fd004");
-                    Message message = new Message();
-                    message.obj = response;
-
-                    Thread.sleep(3000);
-                    handler.sendMessage(message);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                switch (msg.what) {
+                    case 0:
+                        Toast.makeText(getApplicationContext(), "footer", Toast.LENGTH_SHORT).show();
+                        listView.addFooterView(footerView);
+                        break;
+                    case 1:
+                        Gson gson = new Gson();
+                        Type mapType = new TypeToken< ArrayList<Shot> >() {}.getType();
+                        shotArrayList = gson.fromJson((String) msg.obj, mapType);
+                        myAdapter.refresh(shotArrayList);
+                        isloading = false;
+                        //Toast.makeText(getApplicationContext(), "loading", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
                 }
 
             }
-        }).start();
+        };
+
+        Thread thread = new ThreadGetData();
+        thread.start();
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -79,6 +88,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+                if(view.getLastVisiblePosition() == totalItemCount-1 && myAdapter.getCount() >=10 && isloading == false){
+                    isloading = true;
+                    Thread thread = new ThreadGetData();
+                    thread.start();
+                }
+
+            }
+        });
+    }
+
+    public class ThreadGetData extends Thread{
+        @Override
+        public void run() {
+            try {
+                handler.sendEmptyMessage(0);
+
+                apiService apitest = new apiService();
+                String response = apitest.get("https://api.dribbble.com/v1/shots/?access_token=82b6a13113f4406633b6b7f44972a7a74fa4578fd3f08e4f92a80f65690fd004");
+
+                Thread.sleep(3000);
+
+                Message message = handler.obtainMessage(1, response);
+                handler.sendMessage(message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
 }
